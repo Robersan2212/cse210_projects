b0f59da0da51e91314716e7d45bfb536f94e5001
robersan2212 <robertmolina1222@gmail.com>
2024-06-21 19:20:10 -0600

    created shape classes

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,21 @@
+// Program.cs
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 4.0),
+            new Rectangle("Blue", 3.0, 5.0),
+            new Circle("Green", 2.5)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
